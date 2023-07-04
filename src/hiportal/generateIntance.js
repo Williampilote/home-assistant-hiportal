c03ef82c5de19059b548d323f8ea7db4f577114f
Williampilote <57291065+Williampilote@users.noreply.github.com>
2023-07-05 01:38:57 +0200

    Create generateIntance.js

---
+++ src/hiportal/generateIntance.js
@@ -0,0 +1,16 @@
+module.exports = async () => {
+  const puppeteer = require("puppeteer");
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    headless: true,
+  });
+  const page = await browser.newPage();
+  try {
+    await page.goto("https://www.hyponportal.com/signin");
+    return { page, browser };
+  } catch (error) {
+    await browser.close();
+    console.log(error);
+    process.exit(1);
+  }
+};
